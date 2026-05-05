df81a3085a46367d171bfb18e271fbcd4389efe1
adarshaharshavardhana@gmail.com <adarshaharshavardhana@gmail.com>
2026-05-06 00:12:26 +0530

    added the cardAd component1

--- src/newapproach/ListingTypeView.jsx
+++ src/newapproach/ListingTypeView.jsx
@@ -601,8 +601,7 @@ const ListingTypeView = () => {
       
       {/* Property Card Container */}
       <div className="relative overflow-hidden border border-gray-100 group-hover:border-blue-200 transition-all duration-500 group-hover:shadow-2xl shadow-lg ">
-        {/* <div className="absolute inset-0 border-2 border-transparent group-hover:border-blue-100 rounded-3xl transition-all duration-500 pointer-events-none"></div>
-         */}
+      
         <div className="transform group-hover:-translate-y-1 transition-transform duration-500 h-full">
           <PropertyUnitCard 
             propertyUnit={unit}
